91a93fd0741f7cd55c48ec00b33c11cdb2c6ecf1
vamshiaruru <vamshiaruru0@gmail.com>
2017-11-10 23:40:51 +0530

    rmse calculated, 1.04799

--- filter.py
+++ filter.py
@@ -1,6 +1,7 @@
 from __future__ import division
 import numpy as np
 import scipy.sparse as ss
+from datetime import datetime
 
 
 class Filter(object):
@@ -12,6 +13,7 @@ class Filter(object):
     rating_matrix = None
     base_entries = 80000
     test_entries = 20000
+    related_items = {}
 
     def __init__(self):
         self.build_matrix()
@@ -47,17 +49,16 @@ class Filter(object):
         Filter.adjust_vector(user2_vector)
         user1_vector = user1_vector/np.linalg.norm(user1_vector)
         user2_vector = user2_vector / np.linalg.norm(user2_vector)
-
         return user1_vector.dot(user2_vector)
 
     def find_top_k_neighbors(self, item, k=100):
         similarity = []
         for item_id in xrange(self.rating_matrix.shape[0]):
             if not item == item_id:
-                if np.isnan(self.find_cosine_similarity(item, item_id)):
-                    print "this {}".format(item_id)
+                sim = self.find_cosine_similarity(item, item_id)
+                if np.isnan(sim):
                     continue
-                similarity.append(self.find_cosine_similarity(item, item_id))
+                similarity.append(sim)
         similar_vectors = np.array(similarity).argsort()[::-1][0:k]
         return similar_vectors
 
@@ -69,19 +70,51 @@ class Filter(object):
             correction = self.baseline_estimate(user, item)
         else:
             correction = 0
-        similar_items = self.find_top_k_neighbors(item)
+        if item in self.related_items:
+            print "there"
+            similar_items = self.related_items[item]
+        else:
+            similar_items = self.find_top_k_neighbors(item, k=200)
+            self.related_items[item] = similar_items
         denominator = 0
         numerator = 0
         item_vector = self.rating_matrix[item, :].toarray()[0]
         user_rating = np.array([x[0] for x in
                                list(self.rating_matrix[:, user].toarray())])
+        self.adjust_vector(user_rating)
         for i in similar_items:
-            rating = user_rating[i]
+            rated = user_rating[i]
+            if rated == 0:
+                continue
             similarity = self.find_cosine_similarity(item, i)
-            numerator += similarity * rating
+            numerator += similarity * rated
             denominator += similarity
-        return correction + (numerator/denominator) + np.average(item_vector)
+        try:
+            predicted_rating = correction + (numerator/denominator) + np.average(
+                item_vector)
+        except Exception:
+            predicted_rating = correction + np.average(item_vector)
+
+        return predicted_rating
+
 
 if __name__ == "__main__":
     filterObj = Filter()
-    print filterObj.predict_rating(0, 0)
+    print datetime.now().time()
+    rmse = 0
+    try:
+        for j in xrange(filterObj.users.shape[0]):
+            print j, rmse
+            user = filterObj.users[j]
+            item = filterObj.items[j]
+            rating = filterObj.ratings[j]
+            predicted = filterObj.predict_rating(user, item)
+            rmse += (predicted - rating) ** 2
+    except KeyboardInterrupt:
+        print datetime.now().time()
+    rmse = rmse/filterObj.users.shape[0]
+    print rmse ** 0.5
+    print datetime.now().time()
+
+# 20:59:46.446547
+# rmse = 1.04799110545
