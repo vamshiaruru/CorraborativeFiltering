2e418ecb136b90f279849a7f6da848533290b150
vamshiaruru <vamshiaruru0@gmail.com>
2017-11-11 12:19:32 +0530

    more progress

--- filter.py
+++ filter.py
@@ -2,6 +2,8 @@ from __future__ import division
 import numpy as np
 import scipy.sparse as ss
 from datetime import datetime
+import shelve
+from contextlib import closing
 
 
 class Filter(object):
@@ -13,7 +15,8 @@ class Filter(object):
     rating_matrix = None
     base_entries = 80000
     test_entries = 20000
-    related_items = {}
+    related_items = "items_dict.db"
+    mean = 0
 
     def __init__(self):
         self.build_matrix()
@@ -29,11 +32,13 @@ class Filter(object):
                 users[count] = line[0] - 1
                 movies[count] = line[1] - 1
                 ratings[count] = line[2]
+                self.mean += ratings[count]
                 count += 1
         self.rating_matrix = ss.csr_matrix((ratings, (movies, users)))
         self.items = movies
         self.users = users
         self.ratings = ratings
+        self.mean = self.mean/count
 
     @classmethod
     def adjust_vector(cls, vector):
@@ -60,61 +65,101 @@ class Filter(object):
                     continue
                 similarity.append(sim)
         similar_vectors = np.array(similarity).argsort()[::-1][0:k]
-        return similar_vectors
+        similarities = {}
+        for vector in similar_vectors:
+            similarities[vector] = similarity[vector]
+        return similarities
+
+    def average(self, vector):
+        sum = 0
+        count = 0
+        for i in vector:
+            if not i == 0:
+                count += 1
+                sum += i
+
+        try:
+            ave = sum/count
+        except ZeroDivisionError:
+            ave = 0
+        return ave
 
     def baseline_estimate(self, user, item):
-        return 0
+        item_vector = self.rating_matrix[item, :].toarray()[0]
+        item_mean = self.average(item_vector)
+        user_rating = np.array([x[0] for x in
+                                list(self.rating_matrix[:, user].toarray())])
+        user_mean = self.average(user_rating)
+        return self.mean + (user_mean - self.mean) + (item_mean - self.mean)
 
     def predict_rating(self, user, item, baseline=False):
         if baseline:
             correction = self.baseline_estimate(user, item)
         else:
             correction = 0
-        if item in self.related_items:
-            print "there"
-            similar_items = self.related_items[item]
-        else:
-            similar_items = self.find_top_k_neighbors(item, k=200)
-            self.related_items[item] = similar_items
+        related = shelve.open(self.related_items)
+        similar_items = related[str(int(item))]
         denominator = 0
         numerator = 0
-        item_vector = self.rating_matrix[item, :].toarray()[0]
         user_rating = np.array([x[0] for x in
                                list(self.rating_matrix[:, user].toarray())])
-        self.adjust_vector(user_rating)
         for i in similar_items:
             rated = user_rating[i]
             if rated == 0:
                 continue
-            similarity = self.find_cosine_similarity(item, i)
+            similarity = similar_items[i]
+            if baseline:
+                rated = rated - self.baseline_estimate(user, i)
             numerator += similarity * rated
             denominator += similarity
         try:
-            predicted_rating = correction + (numerator/denominator) + np.average(
-                item_vector)
+            predicted_rating = correction + (numerator/denominator)
         except Exception:
-            predicted_rating = correction + np.average(item_vector)
+            predicted_rating = correction
 
         return predicted_rating
 
+    def save_similarities(self):
+        print datetime.now().time()  # 10:20:46.813193
+        with closing(shelve.open(self.related_items)) as db:
+            for j in xrange(1682):
+                print j
+                db[str(j)] = self.find_top_k_neighbors(j)
+        print datetime.now().time()
 
 if __name__ == "__main__":
     filterObj = Filter()
+    test_movies = np.zeros(20000)
+    test_users = np.zeros(20000)
+    test_ratings = np.zeros(20000)
+    count = 0
+    with open(filterObj.test_file, "r") as f:
+        for line in f:
+            line = [int(word) for word in line.strip().split("\t")]
+            test_users[count] = line[0] - 1
+            test_movies[count] = line[1] - 1
+            test_ratings[count] = line[2]
+            count += 1
     print datetime.now().time()
     rmse = 0
+    rmse_baseline = 0
+    count = 0
     try:
-        for j in xrange(filterObj.users.shape[0]):
-            print j, rmse
-            user = filterObj.users[j]
-            item = filterObj.items[j]
-            rating = filterObj.ratings[j]
+        for j in xrange(test_ratings.shape[0]):
+            print j
+            count += 1
+            user = test_users[j]
+            item = test_movies[j]
+            rating = test_ratings[j]
             predicted = filterObj.predict_rating(user, item)
+            predicted_b = filterObj.predict_rating(user, item, baseline=True)
             rmse += (predicted - rating) ** 2
+            rmse_baseline += (predicted_b - rating) ** 2
     except KeyboardInterrupt:
         print datetime.now().time()
     rmse = rmse/filterObj.users.shape[0]
-    print rmse ** 0.5
+    rmse_baseline = rmse/filterObj.users.shape[0]
+    print rmse ** 0.5, rmse_baseline ** 2
     print datetime.now().time()
 
-# 20:59:46.446547
-# rmse = 1.04799110545
+# rmse = 0.530413696952, rmse_baseline = 1.23674153817e-11
